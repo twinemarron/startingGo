0f95294c7a0996a92780cc8c041b0e5509d05eb7
twinemarron <twinemarron@gmail.com>
2019-08-03 04:03:20 +0900

    Modify

--- sampleCode/main.go
+++ sampleCode/main.go
@@ -5,18 +5,49 @@ import (
 )
 
 func main() {
-  // var (
-  //   a1 [5]int
-  //   a2 [5]uint
-  // )
-  const (
-    Z = X + Y
-    X = 2
-    Y = 7
-    // C = A + "は" + B
-    // A = "今日"
-    // B = "雨"
-  )
-  fmt.Println(Z)
-  // fmt.Println(C)
+  ch1 := make(chan int)
+  ch2 := make(chan int)
+  ch3 := make(chan int)
+  go func() {
+    for {
+      i := <-ch1
+      ch2 <- (i * 2)
+    }
+  }()
+  go func() {
+    for {
+      i := <-ch2
+      ch3 <- (i - 1)
+    }
+  }()
+  n := 1
+  LOOP:
+  for {
+    select {
+    case ch1 <- 1:
+      n++
+    case i := <-ch3:
+      fmt.Println("recieved", i)
+    default:
+      if n > 100 {
+        break LOOP
+      }
+    }
+  }
+  // ch1 := make(chan int, 1)
+  // ch2 := make(chan int, 1)
+  // ch3 := make(chan int, 1)
+  // ch1 <- 1
+  // ch2 <- 1
+  //
+  // select {
+  // case <- ch1:
+  //   fmt.Println("ch1から受信")
+  // case <- ch2:
+  //   fmt.Println("ch2から受信")
+  // case ch3 <- 3:
+  //   fmt.Println("ch3から受信")
+  // default:
+  //   fmt.Println("ここへは到達しない")
+  // }
 }
